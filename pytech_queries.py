927e2e91a89e0ff04c8597459f927922a6353985
mommaapril1016 <83374978+mommaapril1016@users.noreply.github.com>
2022-02-07 12:36:28 -0500

    Create pytech_queries.py

---
+++ pytech_queries.py
@@ -0,0 +1,7 @@
+db = client.pytech
+students = db.students
+student_list = students.find({})
+print("\n  -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+student_list:
+    print(" Student ID: " + "\n  First Name")
+  
